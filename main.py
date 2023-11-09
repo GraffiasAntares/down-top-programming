c507bd3d5f1763c7426a89aea3344b3fd1832e60
GraffiasAntares <star211@list.ru>
2023-11-09 10:34:48 +0700

    basic subtasks

---
+++ main.py
@@ -0,0 +1,30 @@
+def int_input(inpt):
+    pass
+
+
+def test_name_date_input():
+    pass
+
+
+def create_test(registry):
+    pass
+
+
+def create_exam(registry):
+    pass
+
+
+def create_graduation_exam(registry):
+    pass
+
+
+def create_test_trial(registry):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
